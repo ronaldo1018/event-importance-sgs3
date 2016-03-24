ce762fab7a2ea49ffb280de8d6134db091d638d5
Yen Chi Hsuan <yan12125@gmail.com>
2016-03-24 23:48:32 +0800

    Copy more files from B2G

--- copy_include_libs.sh
+++ copy_include_libs.sh
@@ -8,8 +8,15 @@ rm -f lib/*.so
 rm -rvf include/frameworks/native/include
 rm -rvf include/system/core/include
 rm -f include/{connector.h,cn_proc.h}
+rm -f include/importance_data.h
+rm -f IImportance.{cpp,h}
+
+mkdir -p lib
+mkdir -p include/{frameworks/native,system/core}
 
 cp $ANDROID_ROOT/out/target/product/*/system/lib/{libbinder.so,libutils.so} lib
 cp -rv $ANDROID_ROOT/frameworks/native/include include/frameworks/native/
 cp -rv $ANDROID_ROOT/system/core/include include/system/core/
+cp $ANDROID_ROOT/gecko/hal/importance_data.h include/
+cp $ANDROID_ROOT/gecko/hal/gonk/IImportance.{cpp,h} .
 cp $KERNEL_ROOT/include/linux/{connector.h,cn_proc.h} include/
